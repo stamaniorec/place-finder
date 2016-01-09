6b7d0d40f024ec49b30b60f2ca6f0d5323f2b75c
stamaniorec <stamaniorec@gmail.com>
2016-01-09 12:47:59 +0200

    Google Places Web Service API with very messy code.

--- place-finder.rb
+++ place-finder.rb
@@ -5,7 +5,23 @@ get '/' do
   erb :index
 end
 
+require 'net/http'
+require 'json'
+
 post '/' do
   @query = params[:search]
-  erb :results
+  key = 'AIzaSyARbPKh6w-tSaeNGX3wpvY_EQoHDIzYgh0'
+  endpoint = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+  url = endpoint + "query=#{@query}&key=#{key}"
+  response = Net::HTTP.get(URI.parse(url))
+  json_response = JSON.parse(response)
+  status = json_response['status']
+  if status != 'OK'
+    p 'whoops something went wrong'
+    erb :error
+  else
+    @suggestions = json_response['results']
+    erb :results
+  end
 end
+
