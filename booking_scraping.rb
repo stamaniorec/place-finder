201e32af06a2096873bea61eba479163ffcff671
stamaniorec <stamaniorec@gmail.com>
2016-01-10 14:21:28 +0200

    Scraping data from Booking.com and refactored a little.

--- booking_scraping.rb
+++ booking_scraping.rb
@@ -1,18 +1,59 @@
 require 'mechanize'
 
 class BookingScraping
-  def get_data(place_name)
-    url = "http://www.booking.com/search.bg.html?si=ai%2Cco%2Cci%2Cre%2Cdi&ss=ephesia%20holiday%20beach/"
-
-    browser = Mechanize.new do |agent|
+  def initialize
+    @browser = Mechanize.new do |agent|
       agent.user_agent_alias = 'Mac Safari'
     end
+  end
 
-    browser.get(url) do |page|
-      page.search('.destination_name').each do |place_name|
-        p place_name.inner_html
-      end
+  def found_place?(page, target)
+    page.search('.destination_name').any? do |place_name|
+      place_name.inner_html == target
     end
+  end
+
+  def get_search_url(place_name)
+    "http://www.booking.com/search.bg.html?si=ai%2Cco%2Cci%2Cre%2Cdi&ss=#{place_name}/"
+  end
+
+  def get_suggestions_page(place_name)
+    url = get_search_url(place_name)
+    @browser.get(url) do |suggestions_page|
+      suggestions_page
+    end
+  end
+
+  def navigate_to_hotel_page(suggestions_page, place_name)
+    link_to_hotel_list = suggestions_page.link_with(text: place_name)
+    hotel_list = link_to_hotel_list.click
+
+    target_place_link = hotel_list.link_with(text: "\n#{place_name}\n")
+    target_place_link.click
+  end
+
+  def get_hotel_page(place_name)
+    suggestions_page = get_suggestions_page(place_name)
+
+    if found_place?(suggestions_page, place_name)
+      navigate_to_hotel_page(suggestions_page, place_name)
+    else
+      :not_found
+    end
+  end
+
+  def get_data(place_name)
+    hotel_page = get_hotel_page(place_name)
+    if hotel_page == :not_found
+      return "not found :("
+    else
+      hotel_page.at('#hp_hotel_name').inner_html
+      # ...
+    end
+    # fill data hash
+    # return data
   end
 end
 
+a = BookingScraping.new
+p a.get_data('Ephesia Holiday Beach Club')
--- place_finder.rb
+++ place_finder.rb
@@ -4,18 +4,25 @@ require 'sinatra/reloader' if development?
 require_relative 'booking_scraping'
 require_relative 'google_places_text_search'
 
+helpers do
+  def get_suggestions(query)
+    GooglePlacesTextSearch.text_search(params[:search])
+  end
+end
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  @suggestions = GooglePlacesTextSearch.text_search(params[:search])
+  @suggestions = get_suggestions(params[:search])
   erb :results
 end
 
 get '/place/:name' do
   @name = params[:name]
   google_places_result = GooglePlacesTextSearch.text_search(@name)
+  # p google_places_result
   @rating = google_places_result.first['rating']
   @booking_data = BookingScraping.new.get_data(@name)
   erb :place
