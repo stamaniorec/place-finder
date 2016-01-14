3f046a45c6fb907276e7779e3ed3d65adb0c5d04
stamaniorec <stamaniorec@gmail.com>
2016-01-14 10:25:54 +0200

    Refactored tests a little. Added two new tests.

--- booking_scraping_spec.rb
+++ booking_scraping_spec.rb
@@ -48,33 +48,54 @@ describe BookingScraping do
   end
 
   describe '#get_stars' do
+    before(:each) do
+      @booking = BookingScraping.new
+      @hotel_page = double
+    end
+
     context 'has star information available' do
       it 'returns the number of stars of the hotel' do
-        booking = BookingScraping.new
-        hotel_page = double
         nokogiri_xml_element = {'title' => '5 звезди'}
-        allow(hotel_page).to receive(:at).with('.star_track').and_return(nokogiri_xml_element)
-        expect(booking.get_stars(hotel_page)).to eq(5)
+        allow(@hotel_page).to receive(:at).with('.star_track').and_return(nokogiri_xml_element)
+        expect(@booking.get_stars(@hotel_page)).to eq(5)
       end
     end
 
     context '.star_track has different inner html from usual' do
       it 'returns 0' do
-        booking = BookingScraping.new
-        hotel_page = double
         nokogiri_xml_element = {'title' => 'Хотел с 5 звезди'}
-        allow(hotel_page).to receive(:at).with('.star_track').and_return(nokogiri_xml_element)
-        expect(booking.get_stars(hotel_page)).to eq(0)
+        allow(@hotel_page).to receive(:at).with('.star_track').and_return(nokogiri_xml_element)
+        expect(@booking.get_stars(@hotel_page)).to eq(0)
       end
     end
 
     context 'there is no element with class star_track in the html document' do
       it 'returns nil' do
-        booking = BookingScraping.new
-        hotel_page = double
         nokogiri_xml_element = {'title' => 'Хотел с 5 звезди'}
-        allow(hotel_page).to receive(:at).with('.star_track').and_return(nil)
-        expect(booking.get_stars(hotel_page)).to eq(nil)
+        allow(@hotel_page).to receive(:at).with('.star_track').and_return(nil)
+        expect(@booking.get_stars(@hotel_page)).to eq(nil)
+      end
+    end
+  end
+
+  describe '#found_place?' do
+    before(:each) do
+      @booking = BookingScraping.new
+      @page = double
+      place = double
+      allow(place).to receive(:inner_html).and_return('foo')
+      allow(@page).to receive(:search).with('.destination_name').and_return([place])
+    end
+
+    context 'target exists in page' do
+      it 'returns true' do
+        expect(@booking.found_place?(@page, 'foo')).to eq(true)
+      end
+    end
+
+    context 'target doe not exist in page' do
+      it 'returns true' do
+        expect(@booking.found_place?(@page, 'bar')).to eq(false)
       end
     end
   end
