8cdd890e85f2798bad51c2cc61a5b3c5afe945e5
stamaniorec <stamaniorec@gmail.com>
2016-01-23 12:15:41 +0200

    More config for db in production

--- place_finder.rb
+++ place_finder.rb
@@ -12,7 +12,7 @@ configure :development do
 end
 
 configure :production do
-  set :database, {adapter: 'postgresql', database: ENV['DATABASE_URL']}
+  set :database, {adapter: 'postgresql', database: ENV['DATABASE_URL'], host: 'localhost', encoding: 'unicode', pool: 5}
 end
 
 require_relative 'models/user'
